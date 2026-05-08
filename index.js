c1155bed374b6d53d5f1dd99e72a91d3fdc6a9e7
Kushal <opkushal7@gmail.com>
2026-05-08 20:26:24 +0530

    added node dependency

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+const cowsay = require('cowsay');
 console.log('Welcome to your first workflow!!!');
+
+console.log( cowsay.say({
+    text : "Hello, DevOps is awsome! Yeah baby!!",
+    e : "OO",
+    T : "U "
+     })
+);
